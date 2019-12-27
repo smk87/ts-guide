d4afdbf184fc49063281fd37df11bb1d109025ed
Saqlain Mahmud <30999133+smk87@users.noreply.github.com>
2019-12-27 19:16:12 +0600

    ADD: When to use type annotations.

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -26,3 +26,31 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// When to annotations
+// 1. Function that return 'any' type
+const json = '{"x":10,"y":20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates);
+
+// 2. When we declare a varaible and initialize on different line
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+    console.log(words[i]);
+  }
+}
+
+// 3. Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < myNumbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
