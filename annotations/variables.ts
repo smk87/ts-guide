c320225b7defe0eb8a4b323d80137a5c997a0a0c
Saqlain Mahmud <sqlnmhmdkhanemon@gmail.com>
2020-03-01 19:18:46 +0600

    ADD: genrics.ts

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -1,5 +1,5 @@
 let apple: number = 5;
-let speed: string = 'fast';
+let speed: string = "fast";
 let hasName: boolean = true;
 let nothingMuch: null = null;
 let nothing: undefined = undefined;
@@ -8,7 +8,7 @@ let nothing: undefined = undefined;
 let now: Date = new Date();
 
 // Array
-let color: string[] = ['red', 'green', 'blue'];
+let color: string[] = ["red", "green", "blue"];
 let myNumbers: number[] = [1, 2, 3];
 let truths: boolean[] = [true, false, true];
 
@@ -27,18 +27,18 @@ const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
 
-// When to annotations
+// When to use Type Annotations
 // 1. Function that return 'any' type
 const json = '{"x":10,"y":20}';
 const coordinates: { x: number; y: number } = JSON.parse(json);
 console.log(coordinates);
 
 // 2. When we declare a varaible and initialize on different line
-let words = ['red', 'green', 'blue'];
+let words = ["red", "green", "blue"];
 let foundWord: boolean;
 
 for (let i = 0; i < words.length; i++) {
-  if (words[i] === 'green') {
+  if (words[i] === "green") {
     foundWord = true;
     console.log(words[i]);
   }
---
+++ generics.ts
@@ -0,0 +1,75 @@
+// Example of genrics with classes
+
+class ArrayOfNumbers {
+  constructor(public collection: number[]) {}
+
+  get(index: number): number {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfStrings {
+  constructor(public collection: string[]) {}
+
+  get(index: number): string {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfAnnything<T> {
+  constructor(public collection: T[]) {}
+
+  get(index: number): T {
+    return this.collection[index];
+  }
+}
+
+const arr = new ArrayOfAnnything<string>(["a", "b", "c"]);
+
+// Example of genrics with functions
+
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything<string>(["a", "b", "c"]);
+
+// Generic Constraints
+class Car {
+  print() {
+    console.log("I am a car");
+  }
+}
+
+class House {
+  print() {
+    console.log("I am a house");
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printHousesOrCars<House>([new House(), new House()]);
+printHousesOrCars<Car>([new Car(), new Car()]);
